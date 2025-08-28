faeb360c71db77a8f667333d7e9bffaada7e099c
nery56-crypto <ccereal97@gmail.com>
2025-08-27 21:15:48 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author ASUS
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // DECLARACION DE VARIABLES
+        // CREARLAS LAS VARIABLES PARA QUE SE PUEDAN USAR
+        //TIPO DE DATO IDENTIFICADOR
+        String nombre;
+        byte edad;
+        //INICIALIZACION
+        //USAMO EL OPERADOR = --> OPERADOR DE ASIGNACIÓN
+        //PON "Alejandro Nery" DENTRO DE LA VARIABLE nombre
+        nombre = "ALEJANDRO NERY";
+        edad = 18;
+        //USO DE VARIABLES
+        System.out.println(nombre);
+        System.out.println(edad);
+                
+    }
+    
+}
